5d668c2f06a985fd26e5007804ff729215d78de1
Justin Chou <87676813+nakschou@users.noreply.github.com>
2024-09-28 15:25:32 -0700

    added boston and colorado

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ api_key = os.environ.get("OUR_API_KEY")
 
 dct = {
     "hawaii": "https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/8c60fedd-e5dd-4ff9-a73b-75656951d393/9e638e85-ce84-4f3f-8b2a-5ef5b20e3955_video02646ea6f897542689b259f573b3f0b31.mp4",
-    "hongkong": "https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/b4be73bd-48b1-483b-b5c1-9f70768da411/8318dded-9dba-435d-aefa-2604f69815f5_video0ff773ea00fdd4435988ed76e770bf912.mp4"}
+    "hongkong": "https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/b4be73bd-48b1-483b-b5c1-9f70768da411/8318dded-9dba-435d-aefa-2604f69815f5_video0ff773ea00fdd4435988ed76e770bf912.mp4",
+    "boston": "https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/3e19cf0b-942d-4d34-beae-bdfe3ace48f6/edf5bd5b-66ce-4fa1-aca1-ba85c8895af9_video0ae9e4655296944319cb6776ea66c564b.mp4",
+    "colorado": "https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/7be059ea-1d7c-4ba3-af1c-c61783625a17/3fee2fa7-5dde-4fa5-ad72-cd829fd656ad_video0aff89037c13341aa93ae73c76d686248.mp4"
+}
 
 def create_custom_response(data, status_code=200, mimetype='application/json'):
     response = Response(
